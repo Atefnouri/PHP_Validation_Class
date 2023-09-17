a9768b4bfdfa2ee19c24115725d042df8df03c24
Atefnouri <nouriatef.isa@gmail.com>
2023-09-17 09:09:02 +0100

    added style sheet

--- index.php
+++ index.php
@@ -1,5 +1,37 @@
-<?php
+<?php 
 
-echo "hello world2";
+
+
+if ( isset($_POST['submit']) ){
+
+   //form validation is here 
+   echo "form is submitted";
+
+}
 
 ?>
+
+<html lang="en">
+<head>
+  <title>PHP OOP</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+  
+  <div class="new-user">
+    <h2>Create a new user</h2>
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+
+      <label>username: </label>
+      <input type="text" name="username">
+
+      <label>email: </label>
+      <input type="text" name="email">
+
+      <input type="submit" value="submit" name="submit">
+
+    </form>
+  </div>
+
+</body>
+</html>
